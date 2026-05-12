115f4870009a6b6225072abc3a63cd3a090500b6
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2026-05-12 10:09:26 -0700

    Add missing dependencies and create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+
+slint::include_modules!();
+
+fn main() -> Result<()> {
+    let main_window = init()?;
+
+    main_window.run().context("slint platform crashed")?;
+
+    Ok(())
+}
+
+fn init() -> Result<MainWindow> {
+    let main_window = MainWindow::new().context("failed to create main window")?;
+    Ok(main_window)
 }
